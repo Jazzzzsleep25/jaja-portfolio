bd8e266d769c869f004afeb4ef565a1b23299088
stephmargallo <janguk1023@gmail.com>
2023-10-03 23:34:23 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Carousel
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
+
+// Material Box
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+});
+
+// Parallax
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+// Sidenav
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
